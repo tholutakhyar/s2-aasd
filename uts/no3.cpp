0af33b1c7bc89b5bdd2ddcead250f669131df4ee
Tholut Akhyar <tholutx@gmail.com>
2022-05-23 15:41:11 +0700

    add comment and fix space

--- uts/no3.cpp
+++ uts/no3.cpp
@@ -11,29 +11,33 @@ int main() {
 	
 	cout  << endl;
 	cout << "[input]" << endl;
+	// input kalimat
 	cout << "masukkan kalimat (max 100 karakter terimasuk spasi): ";
 	gets(input_char);
 	
+	// input karater yang ingin dicari
 	cout << "satu karakter yang ingin dicari: ";
 	cin >> find_char;
 	
 	count_char = strlen(input_char); // menghitung jumlah karakter dari input
 	
+	// proses pencarian karakter satu per satu
 	for (int i = 0; i < count_char ; i++) {
 		if (input_char[i] == find_char) {
 			count_find_char++;
 		}
 	}
 	
+	// jika yang ditemukan lebih dari 0 maka status menjadi ada
 	if (count_find_char > 0) {
 		status = "ada";
 	}
 	
 	cout  << endl;
 	
-	cout << "[output/hasil]" << endl;
+	// hasil
 	cout << "karakter yang dicari: " << find_char << endl;
 	cout << "status: " << status << endl;
-    cout << "jumlah karater " << find_char << ": " << count_find_char << endl;
+	cout << "jumlah karater " << find_char << ": " << count_find_char << endl;
 	cout << "jumlah total karakter: " << count_char << endl;                   	    
 }
